610846c16e6521544ff029cc5b6f35d3bbf120d8
LucaG123 <lgacy1558@bvsd.org>
2016-02-08 15:07:04 -0700

    project form

--- scheduleprogram/scheduler.py
+++ scheduleprogram/scheduler.py
@@ -1,5 +1,5 @@
 # all the imports
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, url_for, redirect
 
 # configuration
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -31,14 +31,17 @@ class CreateProject(Form):
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
+    # username = db.Column(db.String(80), unique=True)
     project = db.Column(db.String(80), unique=True)
+    startdate = db.Column(db.Date)
+    enddate = db.Column(db.Date)
+    modules = db.relationship("Modules")
 
-    # modules = db.relationship("Modules" , backref ='project')
-
-    def __init__(self, username, project):
-        self.username = username
+    def __init__(self, project, startdate, enddate):
+        #self.username = username
         self.project = project
+        self.startdate = startdate
+        self.enddate = enddate
 
     def __repr__(self):
         return '<Project %r>' % self.project
@@ -48,15 +51,15 @@ class Modules(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     modulename = db.Column(db.String(80), unique=True)
     project = db.Column(db.String(80))
-    # tasks = db.relationship("Tasks")
-    # project_id = db.Column(db.Integer, db.ForeignKey('Project.id'))
+    tasks = db.relationship("Tasks")
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
 
 
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     taskname = db.Column(db.String(120))
     module = db.Column(db.String(80))
-    # modules_id = db.Column(db.Integer, db.ForeignKey('Modules.id'))
+    modules_id = db.Column(db.Integer, db.ForeignKey('modules.id'))
 
 
 @app.route('/initdb')
@@ -70,10 +73,24 @@ def page():
 
 
 @app.route('/')
-def page2():
-    query = Project.query.order_by(Project.username)
+def homepage():
+    query = Project.query.order_by(Project.project)
     return render_template('test.html', output=query)
 
+
+@app.route('/cproject')
+def cproject():
+    form = CreateProject()
+    if form.validate_on_submit():
+        projectname = form.name.data
+        startdate = form.startdate.data
+        enddate = form.enddate.data
+        project = Project(projectname, startdate, enddate)
+        db.session.add(project)
+        db.session.commit()
+        flash('Project Created: ' + projectname)
+        return redirect(url_for('homepage'))
+    return render_template('cProject.html', form=form)
 
 if __name__ == '__main__':
     app.run()
